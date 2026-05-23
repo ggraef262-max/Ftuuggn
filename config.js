3df65415d6bb7b8a6dfb456dc072f90b3a9fa3d1
ggraef262-max <ggraef262@gmail.com>
2026-05-23 23:21:35 +0200

    Update and rename config.js to index.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    probot_ids :'282859044593598464',//أيدي بروبوت
-    recipientId : '1258950398451126342',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
-    price : '1',//سعر النسخ
-    serverid : '1286662836059115627',//أيدي سيرفارك
-    token : 'MTI3NDg1MTczNTE5NzcxMjQwNg.GhrJ-A.VRNGk5l9nW4YAX6QZMqD0TiehNjdYk5yOCcFik',//توكن بوتك
-    purchaseLogChannelId: '1507812734849253446', // روم عمليات الشراء
-    logsChannelId: '1507812677471441046' // روم السجلات
-}
